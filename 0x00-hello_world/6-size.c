01232bbc024f786a69f17c9554b9f6ceddea70d9
Eyulala <eyuelnigussie23@gmail.com>
2023-07-17 16:39:29 -0700

    eriuhiru

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(char));
-printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(int));
-printf("size of a long int: %lu bytes(s)\n",(unsigned long)sizeof(long));
-printf("size of a long long int: %lu bytes(s)\n",(unsigned long)sizeof(long long));
-printf("size of a float: %lu bytes(s)\n",(unsigned long)sizeof(float));
+printf("size of a char: %lu byte(s)\n", sizeof(char));
+printf("size of an int: %lu byte(s)\n", sizeof(int));
+printf("size of a long int: %lu byte(s)\n", sizeof(long));
+printf("size of a long long int: %lu byte(s)\n", sizeof(long long));
+printf("size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
 
