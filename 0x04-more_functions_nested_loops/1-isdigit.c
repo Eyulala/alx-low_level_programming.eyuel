36da7b1945e4c460429abcd465a490cda61fe1ea
Eyulala <eyuelnigussie23@gmail.com>
2023-07-25 11:54:13 -0700

    jfuerg

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * _isdigit - A functionn that checks 0 through
+ * _isdigit - A functionn that checks 0 through 9
  *@c: parameter to be checked
  *Return: 1 on success and 0 on failure
  */
