db7cc4a5f354640ed67639bdbd1523235d0f5d8f
Eyulala <eyuelnigussie23@gmail.com>
2023-07-31 16:22:40 -0700

    jkhfe

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,15 +6,15 @@
  * @c: The character to be located.
  *
  * Return: If c is found - a pointer to the first occurence.
- * NULL - If c is not found.
+ * If c is not found - NULL.
  */
 char *_strchr(char *s, char c)
 {
 int index;
 for (index = 0; s[index] >= '\0'; index++)
 {
-	if (s[inddex] == c)
-		return (s = index);
+	if (s[index] == c)
+		return (s + index);
 }
 return ('\0');
 }
